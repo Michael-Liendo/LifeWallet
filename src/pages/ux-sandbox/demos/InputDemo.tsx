23c2c5ae840280c3e19859df1113a5eb4f4b86a8
Daridjcm <daricorreamedina@gmail.com>
2024-06-08 17:15:07 -0500

    feat: use input component

---
+++ src/pages/ux-sandbox/demos/InputDemo.tsx
@@ -0,0 +1,13 @@
+import Input from '../../../components/Input';
+
+export function InputDemo() {
+    return (
+        <>
+            <div>
+                <Input type='text'></Input>
+                <Input type='button'></Input>
+                <Input type='color'></Input>
+            </div>
+        </>
+    )
+}
